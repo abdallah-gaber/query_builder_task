4207e965b6f570be2a8f59da4e78656881194e10
abdallah-gaber <abdallah.gaber@teqnia-tech.com>
2021-12-19 10:59:25 +0200

    Added Api Caller to get users list

---
+++ lib/data/api_caller.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:query_builder_task/models/user_model.dart';
+
+class ApiCaller {
+  Dio dio = Dio();
+
+  Future<List<UserModel>> getAllFakeUsersList(context) async {
+    try {
+      var response = await dio.get(
+        'https://mocki.io/v1/fb545e3a-6df0-4c95-b7ee-0704ffb5ba55',
+      );
+
+      // Decoding Response.
+      var decoded = response.data;
+
+      if (response.statusCode! >= 200 && decoded != null) {
+        List<UserModel> usersList = [];
+        for (var item in decoded) {
+          usersList.add(UserModel.fromJson(item));
+        }
+        return usersList;
+      } else {
+        return [];
+      }
+    } on DioError catch (_) {
+      return [];
+    }
+  }
+}
