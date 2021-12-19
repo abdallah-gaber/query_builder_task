fd292fa4786491729b46aa428836ffa0af4929e1
abdallah-gaber <abdallah.gaber@teqnia-tech.com>
2021-12-19 17:39:43 +0200

    user widget modify shape and shadow

--- lib/ui/widgets/user_widget.dart
+++ lib/ui/widgets/user_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:query_builder_task/common/strings.dart';
 import 'package:query_builder_task/models/user_model.dart';
 
 class UserWidget extends StatelessWidget {
@@ -9,39 +8,42 @@ class UserWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 7),
-      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
-      decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(20))),
-      child: Row(
-        children: [
-          CircleAvatar(
-            backgroundColor: const Color(0xFFE5E5E5),
-            child: Icon(userModel!.gender == "Male"
-                ? Icons.male
-                : Icons.female),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                userModel?.fullName ?? '',
-                style: const TextStyle(fontWeight: FontWeight.w900),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              Text(
-                userModel?.gender ?? '',
-              ),
-            ],
-          ),
-        ],
+    return Card(
+      color: Colors.white,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(20))
+      ),
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 7),
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+        child: Row(
+          children: [
+            CircleAvatar(
+              backgroundColor: const Color(0xFFE5E5E5),
+              child: Icon(userModel!.gender == "Male"
+                  ? Icons.male
+                  : Icons.female),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  userModel?.fullName ?? '',
+                  style: const TextStyle(fontWeight: FontWeight.w900),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  userModel?.gender ?? '',
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
