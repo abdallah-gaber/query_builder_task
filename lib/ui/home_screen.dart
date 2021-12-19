26e69f9b6f88a710882c6fe866167149f58877a6
abdallah-gaber <abdallah.gaber@teqnia-tech.com>
2021-12-19 10:54:00 +0200

    Finish Home UI

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:query_builder_task/ui/widgets/query_item_widget.dart';
 
@@ -10,6 +11,14 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<QueryItemWidget> queryItemWidget = [const QueryItemWidget()];
+  List<String> logicOperators = ["AND", "OR"];
+  String? selectedLogicOperator;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedLogicOperator = logicOperators[0];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,14 +60,52 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               ...(queryItemWidget.map((e) {
                 if (queryItemWidget.indexOf(e) == 0) {
-                  return Container(
-                      width: MediaQuery.of(context).size.width,
-                      padding: const EdgeInsets.all(10),
-                      decoration: const BoxDecoration(
-                        color: Color(0xFFF7F7F7),
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
-                      child: e);
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                          width: MediaQuery.of(context).size.width,
+                          padding: const EdgeInsets.all(10),
+                          decoration: const BoxDecoration(
+                            color: Color(0xFFF7F7F7),
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                          child: e),
+                      if (queryItemWidget.length > 1)
+                        Container(
+                          margin: const EdgeInsets.symmetric(vertical: 10),
+                          width: MediaQuery.of(context).size.width / 4,
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(10.0)),
+                              border:
+                                  Border.all(color: const Color(0xFFE5E5E5))),
+                          child: DropdownButtonHideUnderline(
+                            child: DropdownButton<String>(
+                              isExpanded: true,
+                              items: logicOperators.map((item) {
+                                return DropdownMenuItem<String>(
+                                  child: Text(
+                                    item,
+                                    style: const TextStyle(fontSize: 14),
+                                  ),
+                                  value: item,
+                                );
+                              }).toList(),
+                              value: selectedLogicOperator,
+                              onChanged: (value) {
+                                setState(() {
+                                  selectedLogicOperator = value;
+                                });
+                              },
+                            ),
+                          ),
+                        )
+                    ],
+                  );
                 } else {
                   return Container(
                     width: MediaQuery.of(context).size.width,
@@ -84,7 +131,25 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                   );
                 }
-              }).toList())
+              }).toList()),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 20),
+                width: MediaQuery.of(context).size.width,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: const Color(0xFF2D9CDB),
+                  ),
+                  child: TextButton(
+                      onPressed: () {},
+                      child: const Center(
+                        child: Icon(
+                          CupertinoIcons.search,
+                          color: Colors.white,
+                        ),
+                      )),
+                ),
+              ),
             ],
           ),
         ),
--- lib/ui/widgets/query_item_widget.dart
+++ lib/ui/widgets/query_item_widget.dart
@@ -28,6 +28,14 @@ class _QueryItemWidgetState extends State<QueryItemWidget> {
   String? selectedOperator;
   TextEditingController dataController = TextEditingController();
 
+
+  @override
+  void initState() {
+    super.initState();
+    selectedColumn = columnsNames[0];
+    selectedOperator = stringOperators[0];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -35,38 +43,50 @@ class _QueryItemWidgetState extends State<QueryItemWidget> {
         Row(
           children: [
             Expanded(
+                flex: selectedColumn == null || selectedColumn == "Age" ? 2 : 1,
                 child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-                  border: Border.all(color: const Color(0xFFE5E5E5))),
-              child: DropdownButtonHideUnderline(
-                child: DropdownButton<String>(
-                  isExpanded: true,
-                  items: columnsNames.map((item) {
-                    return DropdownMenuItem<String>(
-                      child: Text(item),
-                      value: item,
-                    );
-                  }).toList(),
-                  value: selectedColumn,
-                  hint: const Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Text("Column Name"),
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(10.0)),
+                      border: Border.all(color: const Color(0xFFE5E5E5))),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      items: columnsNames.map((item) {
+                        return DropdownMenuItem<String>(
+                          child: Text(
+                            item,
+                            style: const TextStyle(fontSize: 14),
+                          ),
+                          value: item,
+                        );
+                      }).toList(),
+                      value: selectedColumn,
+                      // hint: const Padding(
+                      //   padding: EdgeInsets.all(8.0),
+                      //   child: Text(
+                      //     "Column Name",
+                      //     style: TextStyle(fontSize: 14),
+                      //   ),
+                      // ),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedOperator = value == "Age"
+                              ? numberOperators[0]
+                              : stringOperators[0];
+                          selectedColumn = value;
+                        });
+                      },
+                    ),
                   ),
-                  onChanged: (value) {
-                    setState(() {
-                      selectedColumn = value;
-                    });
-                  },
-                ),
-              ),
-            )),
+                )),
             const SizedBox(
               width: 10,
             ),
             Expanded(
+              flex: 1,
               child: Container(
                 padding: const EdgeInsets.symmetric(horizontal: 10),
                 decoration: BoxDecoration(
@@ -83,15 +103,21 @@ class _QueryItemWidgetState extends State<QueryItemWidget> {
                                 : stringOperators))
                         .map((item) {
                       return DropdownMenuItem<String>(
-                        child: Text(item),
+                        child: Text(
+                          item,
+                          style: const TextStyle(fontSize: 14),
+                        ),
                         value: item,
                       );
                     }).toList(),
                     value: selectedOperator,
-                    hint: const Padding(
-                      padding: EdgeInsets.all(12.0),
-                      child: Text("Operator"),
-                    ),
+                    // hint: const Padding(
+                    //   padding: EdgeInsets.all(8.0),
+                    //   child: Text(
+                    //     "Operator",
+                    //     style: TextStyle(fontSize: 14),
+                    //   ),
+                    // ),
                     onChanged: (value) {
                       setState(() {
                         selectedOperator = value;
