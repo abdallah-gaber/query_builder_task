af6a3d914a1bdd54c3f54950b666fdbf1fdae6ea
AbdallahGaber <abdallah.gaber@teqnia-tech.com>
2022-03-02 01:21:49 +0200

    Changed Font Family

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ library query_builder;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:query_builder_task/ui/home_screen.dart';
 import 'data/api_caller.dart';
 import 'models/user_model.dart';
@@ -23,6 +24,9 @@ class MyApp extends StatelessWidget {
       title: 'Query Builder',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.outfitTextTheme(
+          Theme.of(context).textTheme, // If this is not set, then ThemeData.light().textTheme is used.
+        ),
       ),
       home: const HomeScreen(),
     );
